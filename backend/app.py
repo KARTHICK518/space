87c4a3f5bd157d5e8b1af0c3ee12931136d56ba4
KARTHICK518 <karthickrajavel857@gmail.com>
2026-04-29 20:12:59 +0530

    update db config for render deployment

--- backend/app.py
+++ backend/app.py
@@ -8,7 +8,10 @@ CORS(app)
 
 # Database Configuration
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'space.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    "DATABASE_URL",
+    'sqlite:///' + os.path.join(basedir, 'space.db')
+)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
